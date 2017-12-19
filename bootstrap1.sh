8f43404c97d127b305ae77caae021254ac17310f
BinduC27 <jas.bindu27@gmail.com>
2017-12-20 01:12:10 +0530

    Add files via upload

--- bootstrap1.sh
+++ bootstrap1.sh
@@ -6,18 +6,11 @@ export USER_DATA_ENV="/var/cfgsvc/user_data.env"
 export CLOUD_PREFIX=""
 
 export AUTHORIZED_KEYS="/home/$USER_NAME/.ssh/authorized_keys"
-
-export SSH_TESTING_KEYS="/var/cfgsvc_config/authorized_keys_testing"
-export SSH_STAGING_KEYS="/var/cfgsvc_config/authorized_keys_staging"
-export SSH_PRODUCTION_KEYS="/var/cfgsvc_config/authorized_keys_production"
-
-export TESTING_KEYS="/var/cfgsvc_config/testing.env"
-export STAGING_KEYS="/var/cfgsvc_config/staging.env"
-export PRODUCTION_KEYS="/var/cfgsvc_config/production.env"
-
+export SSH_KEYS="/var/cfgsvc/authorized_keys"
+export SECRETS_KEYS="/var/cfgsvc/secret.env"
 export ENVIRONMENT_KEYS="/etc/environment"
 export BOOTSTRAP_DIR="/var/cfgsvc"
-export ECR_KEYS="/var/cfgsvc_config/ecr.env"
+export ECR_KEYS="/var/cfgsvc/ecr.env"
 export BUILD_FILE="/var/cfgsvc/build"
 export BOOTSTRAP_FILE="/var/cfgsvc/bootstrapped_cfgsvc"
 
@@ -73,57 +66,72 @@ sudo echo "127.0.0.1 $VMNAME" >> /etc/hosts
 if [ ! -d "$BOOTSTRAP_DIR"  ]; then
   mkdir $BOOTSTRAP_DIR
   echo "MAIN director \" cfgsvc \" is created " 
- else:
-   bootstrap_status=1 
+ else
+   bootstrap_status="1"
+   echo "bootstraping is done, all required are already present in /var/cfgsvc" 
 fi
 echo "--------------------------------------------------------------------------------------"
-echo " setting the authorization keys based on the envirnoment"
+#echo " setting the authorization keys based on the envirnoment"
 cp $CLOUD_USER_DATA_FILE $USER_DATA_ENV
 
- 
+bootstrap_status="0"
 if [[ "$bootstrap_status" == "0" ]];then
 case "$environment" in
 	    "testing")
-            curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/get_secrets.sh  -o $BOOTSTRAP_DIR/get_secrets.sh
-            chmod 777 $BOOTSTRAP_DIR/get_secrets.sh
-            bash $BOOTSTRAP_DIR/get_secrets.sh "TST" 
-            echo " deployment type is testing , setting the requied ssh keys and secrets to env "
-	    cat $SSH_TESTING_KEYS > $AUTHORIZED_KEYS
-            cat $TESTING_KEYS >> $ENVIRONMENT_KEYS
-            echo "AUTHORIZED_KEYS_PATH:$AUTHORIZED_KEYS"
-            echo "SECRETS_KEYS_PATH : $TESTING_KEYS"
-            echo "SSHKEYS_PATH: $SSH_TESTING_KEYS"
+             echo " deployment type is testing , setting the requied ssh keys and secrets to env "
+             if [ ! -f "$BOOTSTRAP_DIR/get_secrets.sh"  ]; then 
+                echo "Get_secret not exist , hence pulling"
+                curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/get_secrets.sh  -o $BOOTSTRAP_DIR/get_secrets.sh
+                sleep3
+             fi
+             chmod 777 $BOOTSTRAP_DIR/get_secrets.sh
+             echo "executing get_secrets ......"
+             bash $BOOTSTRAP_DIR/get_secrets.sh "TST" 
+             sleep 2
 	    shift # past argument
 	    ;;
 	   "staging")
-            curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/get_secrets.sh  -o $BOOTSTRAP_DIR/get_secrets.sh
+            echo " deployment type is staging, setting the requied ssh keys and secrets to env "
+            if [ ! -f "$BOOTSTRAP_DIR/get_secrets.sh"  ]; then
+             echo "Get_secret not exist , hence pulling"
+             curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/get_secrets.sh  -o $BOOTSTRAP_DIR/get_secrets.sh
+             sleep 3
+            fi
             chmod 777 $BOOTSTRAP_DIR/get_secrets.sh
+            echo "executing get_secrets ......"
+            sleep 3
             bash $BOOTSTRAP_DIR/get_secrets.sh "STG"
-            echo "deployment type is staging , setting the required ssh keys and secrets to env "
-	    cat $SSH_STAGING_KEYS > $AUTHORIZED_KEYS
-            cat $STAGING_KEYS >> $ENVIRONMENT_KEYS
-            echo "AUTHORIZED_KEYS_PATH:$AUTHORIZED_KEYS"
-            echo "SECRETS_KEYS_PATH :$STAGING_KEYS"
-            echo "SSHKEYS_PATH: $SSH_STAGING_KEYS"
+            sleep 2
 
             echo "MSG_CA_CERTS_FILE_PATH='/etc/ssl/certs/ca-certificates.crt'">> $USER_DATA_ENV
 	    shift # past argument
 	    ;;
 	    "production")
-            curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/get_secrets.sh  -o $BOOTSTRAP_DIR/get_secrets.sh
-            chmod 777 $BOOTSTRAP_DIR/get_secrets.sh
-            bash$BOOTSTRAP_DIR/get_secrets.sh "PRD"
-            echo "deployment type is production , setting the required ssh keys and secrets to env"
-	    cat $SSH_PRODUCTION_KEYS > $AUTHORIZED_KEYS
-            cat $PRODUCTION_KEYS >> $ENVIRONMENT_KEYS
-            echo "AUTHORIZED_KEYS_PATH:$AUTHORIZED_KEYS"
-            echo "SECRETS_KEYS_PATH :$PRODUCTION_KEYS"
-            echo "SSHKEYS_PATH: $SSH_PRODUCTION_KEYS"
+               echo " deployment type is production, setting the requied ssh keys and secrets to env "
+               if [ ! -f "$BOOTSTRAP_DIR/get_secrets.sh"  ]; then
+                  curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/get_secrets.sh  -o $BOOTSTRAP_DIR/get_secrets.sh
+                  sleep 3
+              fi
+              chmod 777 $BOOTSTRAP_DIR/get_secrets.sh
+              echo "executing get_secrets ......"
+               bash $BOOTSTRAP_DIR/get_secrets.sh "PRD"
             echo "MSG_CA_CERTS_FILE_PATH='/etc/ssl/certs/ca-certificates.crt'">> $USER_DATA_ENV
 	    shift
 	    ;;
 esac
 fi
+
+echo "AUTHORIZED_KEYS_PATH:$AUTHORIZED_KEYS"
+echo "SECRETS_KEYS_PATH :$PRODUCTION_KEYS"
+echo "SSHKEYS_PATH: $SSH_PRODUCTION_KEYS"
+
+if [ -f  $SSH_KEYS ]; then 
+ cat $SSH__KEYS > $AUTHORIZED_KEYS
+fi 
+if [ -f $SECRETS_KEYS ]; then 
+ cat $SECRETS_KEYS >> $ENVIRONMENT_KEYS
+fi
+
 fileecount=`find $BOOTSTRAP_DIR -type f | wc -l`
 if [[ "$fileecount" == "1" ]]; then
 echo ""  #    rm -rf /var/cfgsvc_config
@@ -132,19 +140,20 @@ fi
 echo "-------------------------------sourcing the the env files---------------------------"
 cat $ECR_KEYS >> $ENVIRONMENT_KEYS
 source /etc/environment
-
-
+echo "the userdata are  are copied to /etc/env"
+exit 1
 echo "-------------------------pulling the script---------------------------------------------"
 
 if [[ "$pop_type" == "mgmt" ]];then
    echo "pulling mgmt pop install script "
    if [ -f $BOOTSTRAP_DIR/install_mp.sh ]; then bash $BOOTSTRAP_DIR/install_mp.sh ; fi
    #if [  -f $BOOTSTRAP_DIR/install_mp.sh  ]; then echo "removing the existing files ";rm -rf $BOOTSTRAP_DIR/install_mp.sh; fi
-  if [ -f "bootstrap_status"  == "0"]; then 
+  if [[  "bootstrap_status"  == "0" ]]; then 
       curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/install_mp.sh  -o $BOOTSTRAP_DIR/install_mp.sh
+   fi
       chmod 777 $BOOTSTRAP_DIR/install_mp.sh
       bash $BOOTSTRAP_DIR/install_mp.sh
-  fi
+  
 fi
 if [[ "$pop_type" == "data" ]];then
    echo "pulling data pop install script "
