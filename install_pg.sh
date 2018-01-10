3f5456fd5e004fd36ccb43bc9f0a44ac7c3fb7a8
BinduC27 <jas.bindu27@gmail.com>
2018-01-10 17:15:29 +0530

    Update install_pg.sh

--- install_pg.sh
+++ install_pg.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 source $SECRETS_KEYS
+echo "dppass: $DB_PASS"
 docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=$DB_PASS -e POSTGRES_DB=mpsdb -e  POSTGRES_USER=mpsroot --name dp_db_cfgsvc  postgres
