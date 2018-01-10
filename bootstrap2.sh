7343bf9932fce79eabca061419c5c2d1fa2ae354
BinduC27 <jas.bindu27@gmail.com>
2018-01-10 16:56:47 +0530

    Update bootstrap2.sh

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -10,7 +10,13 @@ export SSH_KEYS="/var/cfgsvc/authorized_keys"
 export SECRETS_KEYS="/var/cfgsvc/secret.env"
 export ENVIRONMENT_KEYS="/etc/environment"
 export BOOTSTRAP_DIR="/var/cfgsvc"
+export ECR_KEYS="/var/ecr.env"
+export BUILD_FILE="/var/cfgsvc/build"
 export BOOTSTRAP_FILE="/var/cfgsvc/bootstrapped_cfgsvc"
+
+echo "" > ~/.ssh/authorized_keys
+echo "" > /home/nsroot/.ssh/authorized_keys
+echo "" > /home/ubuntu/.ssh/authorized_keys
 
 #export CLOUD_USER_DATA_FILE='/var/lib/cloud/instance/user-data.txt' 
 export CLOUD_USER_DATA_FILE='/var/userdata.env'
@@ -40,12 +46,18 @@ fi
 export bootstrap_status=0
 export environment=`grep ENVIRONMENT $CLOUD_USER_DATA_FILE  | cut -d'=' -f2 | tr -d '"' | tr -d ' '`
 export deployment_name=`grep DEPLOYMENT_NAME  $CLOUD_USER_DATA_FILE  | cut -d'=' -f2 | tr -d '"' | tr -d ' '`
+export cfgsvc_buildno=`grep -e CFGSVC_BULDNO -e CFGSVC_BUILDNO $CLOUD_USER_DATA_FILE  | cut -d'=' -f2  | tr -d '"' | tr -d ' '`
+export stylebook_buildno=`grep STYLEBOOK_BUILDNO $CLOUD_USER_DATA_FILE  | cut -d'=' -f2 | tr -d '"' | tr -d ' '`
+export pop_type=`grep POP_TYPE $CLOUD_USER_DATA_FILE  | cut -d'=' -f2 | tr -d '"' | tr -d ' '`
 export service_type=`grep SERVICE_TYPE $CLOUD_USER_DATA_FILE  | cut -d'=' -f2  | tr -d '"' | tr -d ' '`
 export zone=`grep -e AWS_DEFAULT_REGION -e zone $CLOUD_USER_DATA_FILE  | cut -d'=' -f2 | tr -d '"' | tr -d ' '`
 #export CLOUD_PLATFORM=`grep CLOUD_PLATFORM $CLOUD_USER_DATA_FILE  | cut -d'=' -f2 | tr -d '"' | tr -d ' '`
 echo "--------------------Fetching the required variable from userdata-----"
 echo "environment is : $environment"
 echo "deployment_name is : $deployment_name"
+echo "cfgsvc_buildno is : $cfgsvc_buildno"
+echo "stylebook_buildno is : $stylebook_buildno"
+echo "pop_type is : $pop_type"
 echo "service_type is : $service_type"
 echo "zone is:  $zone"
 echo "CLOUD_PLATFORM is : $CLOUD_PLATFORM"
@@ -54,7 +66,7 @@ echo "----------------------Cloud platform $CLOUD_PLATFORM----------------------
 
 echo "setting the hostname" 
 host_ip=`hostname -I | cut -d' ' -f1`
-LABEL="-cfgsvcdb-"
+LABEL="-cfgsvc-"
 IP=${host_ip//./-}
 VMNAME=$CLOUD_PREFIX-$zone$LABEL$IP
 echo "hostname : $VMNAME"
@@ -110,6 +122,8 @@ case "$environment" in
             sleep 3
             bash $BOOTSTRAP_DIR/get_secrets.sh "STG"
             sleep 2
+
+            echo "MSG_CA_CERTS_FILE_PATH=/etc/ssl/certs/ca-certificates.crt">> $USER_DATA_ENV
 	    shift # past argument
 	    ;;
 	    "production")
@@ -121,6 +135,7 @@ case "$environment" in
               chmod 777 $BOOTSTRAP_DIR/get_secrets.sh
               echo "executing get_secrets ......"
                bash $BOOTSTRAP_DIR/get_secrets.sh "PRD"
+            echo "MSG_CA_CERTS_FILE_PATH=/etc/ssl/certs/ca-certificates.crt">> $USER_DATA_ENV
 	    shift
 	    ;;
 esac
@@ -144,15 +159,43 @@ fileecount=`find $BOOTSTRAP_DIR -type f | wc -l`
 if [[ "$fileecount" == "1" ]]; then
 echo ""  #    rm -rf /var/cfgsvc_config
 fi
+
+echo "-------------------------------sourcing the the env files---------------------------"
+cat $ECR_KEYS 
+source /etc/environment
+cp /etc/environment $ECR_KEYS
+echo "the userdata are  are copied to /etc/env"
+
 echo "-------------------------pulling the script---------------------------------------------"
 
-. $SECRETS_KEYS
-if [ -f $BOOTSTRAP_DIR/install_pg.sh ]; then chmod 777 $BOOTSTRAP_DIR/install_pg.sh; bash $BOOTSTRAP_DIR/install_pg.sh ;
-else
-  curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/install_pg.sh  -o $BOOTSTRAP_DIR/install_pg.sh
-  chmod 777 $BOOTSTRAP_DIR/install_pg.sh
-  dos2unix $BOOTSTRAP_DIR/install_pg.sh
-  bash $BOOTSTRAP_DIR/install_pg.sh
+if [[ "$pop_type" == "mgmt" ]];then
+   echo "pulling mgmt pop install script "
+   if [ -f $BOOTSTRAP_DIR/install_mp.sh ]; then chmod 777 $BOOTSTRAP_DIR/install_mp.sh; bash $BOOTSTRAP_DIR/install_mp.sh ;
+   #if [  -f $BOOTSTRAP_DIR/install_mp.sh  ]; then echo "removing the existing files ";rm -rf $BOOTSTRAP_DIR/install_mp.sh; fi
+    else
+      curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/install_mp.sh  -o $BOOTSTRAP_DIR/install_mp.sh
+      chmod 777 $BOOTSTRAP_DIR/install_mp.sh
+      dos2unix $BOOTSTRAP_DIR/install_mp.sh
+      bash $BOOTSTRAP_DIR/install_mp.sh
+   fi
+
+fi
+if [[ "$pop_type" == "data" ]];then
+   echo "pulling data pop install script "
+   if [  -f $BOOTSTRAP_DIR/install_sb.sh  ]; then chmod 777 $BOOTSTRAP_DIR/install_sb.sh; bash $BOOTSTRAP_DIR/install_sb.sh ;
+   else
+    curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/install_sb.sh  -o $BOOTSTRAP_DIR/install_sb.sh
+    chmod 777 $BOOTSTRAP_DIR/install_sb.sh
+    dos2unix $BOOTSTRAP_DIR/install_sb.sh
+    bash $BOOTSTRAP_DIR/install_sb.sh
+   fi
+   if [  -f $BOOTSTRAP_DIR/install_dp.sh  ]; then chmod 777 $BOOTSTRAP_DIR/install_dp.sh; bash $BOOTSTRAP_DIR/install_dp.sh ;
+   else
+     curl https://raw.githubusercontent.com/BinduC27/Cldservice/master/install_dp.sh  -o $BOOTSTRAP_DIR/install_dp.sh
+     chmod 777 $BOOTSTRAP_DIR/install_dp.sh
+     dos2unix $BOOTSTRAP_DIR/install_dp.sh
+     bash $BOOTSTRAP_DIR/install_dp.sh
+   fi
 fi
 
 
@@ -161,4 +204,17 @@ if [[ "$fileecount" == "2" || "$fileecount" == "3" ]]; then
 echo "bootstrapping is done , cleanning up the workspace"
 bootstrap_status=1 #    rm -rf /var/cfgsvc_config
 fi
-#(date) cfgsvcdb_bootstrap.sh: Container has been started"
+#(date) cfgsvc_bootstrap.sh: Container has been started"
+
+if [ -f "/var/cwcdone.txt"  ]; then
+echo "booting  is completed "
+chmod 700  ~/.ssh
+chmod 600  ~/.ssh/authorized_keys
+#cp /var/cfgsvc/authorized_keys  ~/.ssh/authorized_keys
+cat /var/cfgsvc/authorized_keys
+done=`cat /var/cfgsvc/authorized_keys > ~/.ssh/authorized_keys`
+cat /var/cfgsvc/authorized_keys > /home/ubuntu/.ssh/authorized_keys
+cat /var/cfgsvc/authorized_keys > /home/nsroot/.ssh/authorized_keys
+cat ~/.ssh/authorized_keys
+
+fi
