5cf6ce1892e97ce6a86933d094a6345535b235b7
BinduC27 <jas.bindu27@gmail.com>
2018-01-15 12:53:01 +0530

    Update get_secrets.sh

--- get_secrets.sh
+++ get_secrets.sh
@@ -34,9 +34,7 @@ docker rm $(docker ps -aq)
 docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=pguser -e POSTGRES_DB=mpsdb -e  POSTGRES_USER=mpsroot --name localdb postgres 
 #docker run -d -it --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -e TRUST_SVC_AUTH="CWSAuth"   -e MESSAGING_SERVER_URL="djehdje" --env-file /var/ecr.env -e DB_USER=mpsroot  -e DB_PORT=5432 -e DB_HOST=$CURR_HOST -e DB_PASS=pguser -e DB_NAME=mpsdb  -p 5000:5000 -p 5002:5002 --name cwcget $cwc_image
 docker run -d -it --restart=always  --env-file /var/ecr.env  -p 5000:5000  --name cwcget $cwc_image
-if [[ "$platform" == "azure" ]]; then 
-docker wait cwcget
-fi
+
 sleep 5
 docker ps
 #docker wait cwcget
