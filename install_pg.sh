d663ff9e72590af9420d0a1e9fee415b02c90683
BinduC27 <jas.bindu27@gmail.com>
2018-01-22 22:19:25 +0530

    Update install_pg.sh

--- install_pg.sh
+++ install_pg.sh
@@ -3,12 +3,7 @@ source $SECRETS_KEYS
 echo "dppass: $DB_PASS"
 docker stop dp_db_cfgsvc
 docker rm dp_db_cfgsvc
-echo -e "cfgsvc\ncfgsvc" | (adduser cfgsvcdb)
-echo -e "cfgsvc123\ncfgsvc123" | (passwd cfgsvcdb)
-sed -i "s/PasswordAuthentication*/PasswordAuthentication yes/g" /etc/ssh/sshd_config > /etc/ssh/sshd_config
-sed "s/PermitRootLogin */PermitRootLogin yes/g" /etc/ssh/sshd_config > /etc/ssh/sshd_config
-usermod -aG sudo cfgsvcdb
-service ssh restart
+
 
 echo "------------------------------list of available user---------------------------------"
 awk -F':' '{ print $1}' /etc/passwd
