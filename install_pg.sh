c5384430f7eaae1589df96b51945a64c37369408
BinduC27 <jas.bindu27@gmail.com>
2018-01-22 22:43:33 +0530

    Update install_pg.sh

--- install_pg.sh
+++ install_pg.sh
@@ -9,4 +9,4 @@ echo "------------------------------list of available user----------------------
 awk -F':' '{ print $1}' /etc/passwd
 echo "-----------------------------------------------------------------------------------------"
 if [[ -n "$DB_PASS" ]]; then echo "The password is $DB_PASS"; else  export DB_PASS="pguser123"; fi
-docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=pguser123 -e POSTGRES_DB=mpsdb -e  POSTGRES_USER=mpsroot --name dp_db_cfgsvc  postgres
+docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=$DB_PASS -e POSTGRES_DB=mpsdb -e  POSTGRES_USER=mpsroot --name dp_db_cfgsvc  postgres
